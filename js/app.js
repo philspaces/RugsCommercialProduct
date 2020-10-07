9cc63df19ba81651e7136b4409a1465afef6ba90
evansnguyen <long.np68@gmail.com>
2020-10-07 23:17:00 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+$(window).on("load", function () {
+    $(".preload").fadeOut(2500);
+});
+
+
+//fields
+masks = document.querySelectorAll(".mask");
+maskContents = document.querySelectorAll(".mask-content");
+
+aboutusImgWrappers = document.querySelectorAll(".img-wrapper");
+aboutusImgInfo = document.querySelectorAll(".mask-aboutus");
+
+//functions
+for (let index = 0; index < masks.length; index++) {
+    const element = masks[index];
+    element.onmouseover = function(){
+        maskContents[index].classList.add("slideUp")
+    }
+    element.onmouseout = function(){
+        maskContents[index].classList.remove("slideUp")
+    }
+}
+
+for (let index = 0; index < aboutusImgWrappers.length; index++) {
+    const element = aboutusImgWrappers[index];
+    element.onmouseover = function(){
+        aboutusImgInfo[index].classList.add("showInfo")
+    }
+    element.onmouseout = function(){
+        aboutusImgInfo[index].classList.remove("showInfo")
+    }
+}
